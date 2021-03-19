7bdc36b621a7acf15e5d382a0cae074cc49d4214
Skeletune <74779780+Skeletune@users.noreply.github.com>
2021-03-19 17:37:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask Heroku"
+    return "Привет от приложения Flask Heroku от слова хрень"
 
 
 if __name__ == '__main__':
